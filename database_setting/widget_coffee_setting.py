0655e9814e21427be5bc514758424ae7d96094c9
siul1024 <hnong09@gmail.com>
2019-12-11 09:33:49 +0900

    widget_coffee_setting QMessageBox update

--- database_setting/widget_coffee_setting.py
+++ database_setting/widget_coffee_setting.py
@@ -1,5 +1,5 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QMessageBox
 from db_connection.coffee_init_service import DBInitService
 
 
@@ -8,8 +8,19 @@ class Ui_coffee(QWidget):
         super().__init__()
         self.DB = DBInitService()
         self.ui = uic.loadUi("database_setting/coffee.ui")
-        self.ui.btn_init.clicked.connect(self.DB.database_init_service)
-        self.ui.btn_restore.clicked.connect(self.DB.database_restore_service)
-        self.ui.btn_backup.clicked.connect(self.DB.database_backup_service)
+        self.ui.btn_init.clicked.connect(self.db_init_service)
+        self.ui.btn_restore.clicked.connect(self.db_restore_service)
+        self.ui.btn_backup.clicked.connect(self.db_backup_service)
         self.ui.show()
 
+    def db_init_service(self):
+        self.ui.btn_init.clicked.connect(self.DB.database_init_service)
+        QMessageBox.about(self, 'init', 'init')
+
+    def db_restore_service(self):
+        self.ui.btn_restore.clicked.connect(self.DB.database_restore_service)
+        QMessageBox.about(self, 'restore', 'restore')
+
+    def db_backup_service(self):
+        self.ui.btn_backup.clicked.connect(self.DB.database_backup_service)
+        QMessageBox.about(self, 'backup', 'backup')
